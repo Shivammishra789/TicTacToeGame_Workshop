b115c1ea9d7bdc178b939c14b1995480feaeb806
Shivammishra789 <shivammishra7789@gmail.com>
2021-10-11 15:28:12 +0530

    UC1 creating board with empty space

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -3,8 +3,20 @@ package com.workshop.tictactoegame;
 public class TicTacToeGame {
 	
 	public static void main(String[] args) {
-	
-		System.out.println("Tic Toc game");
+		
+		System.out.println("Tic Tac Toe game");
+		board();
 	}
 	
-}
+		// creating a board to play
+		static void board() {
+			 char[] board = new char[10];           // declaring array board
+			 
+		// giving each array index value as empty initially
+	    for(int i = 1; i < 10; i++) {
+	    		board[i] = ' ';
+	    	}
+		}
+	}
+	
+
